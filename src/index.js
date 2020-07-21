a47fcef7e2126f3bb6bc8a2909551292258ff6b0
Naman Jindal <namanjindal113@gmail.com>
2020-07-21 13:53:28 +0530

    added redux store, reducer and action template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movie from './reducers/index';
 
+const store = createStore(movie);
+console.log("store",store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
